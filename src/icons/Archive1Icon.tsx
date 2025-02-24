7f351e23fa395ecb2c6147cbc1dc97ba71f0a04f
Blair McKee <23467862+eblairmckee@users.noreply.github.com>
2025-02-24 10:20:28 -0800

    Update archive-1 icon

---
+++ src/icons/Archive1Icon.tsx
@@ -0,0 +1,41 @@
+import React, { forwardRef, type SVGProps } from "react";
+
+const Archive1Icon = forwardRef<SVGSVGElement, SVGProps<SVGSVGElement>>(
+  (props, ref) => (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      ref={ref}
+      {...props}
+    >
+      <path
+        d="M19.5 10.22V19C19.5 21 19 22 16.5 22H7.5C5 22 4.5 21 4.5 19V10.22"
+        stroke="var(--icon-stroke, inherit)"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        d="M5 2H19C21 2 22 3 22 5V7C22 9 21 10 19 10H5C3 10 2 9 2 7V5C2 3 3 2 5 2Z"
+        stroke="var(--icon-stroke, inherit)"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        d="M10.18 14H13.82"
+        stroke="var(--icon-stroke, inherit)"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+    </svg>
+  ),
+);
+
+Archive1Icon.displayName = "Archive1Icon";
+
+export default Archive1Icon;
