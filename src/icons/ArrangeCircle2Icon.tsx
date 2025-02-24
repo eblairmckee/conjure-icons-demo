72477b13dd934fb06b264cd39867b4d8450b770a
Blair McKee <23467862+eblairmckee@users.noreply.github.com>
2025-02-24 10:20:36 -0800

    Update arrange-circle-2 icon

---
+++ src/icons/ArrangeCircle2Icon.tsx
@@ -0,0 +1,59 @@
+import React, { forwardRef, type SVGProps } from "react";
+
+const ArrangeCircle2Icon = forwardRef<SVGSVGElement, SVGProps<SVGSVGElement>>(
+  (props, ref) => (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      ref={ref}
+      {...props}
+    >
+      <path
+        d="M17.15 13.8201L14.11 16.86"
+        stroke="var(--icon-stroke, inherit)"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        d="M6.84998 13.8201H17.15"
+        stroke="var(--icon-stroke, inherit)"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        d="M6.84998 10.18L9.88998 7.14001"
+        stroke="var(--icon-stroke, inherit)"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        d="M17.15 10.1799H6.84998"
+        stroke="var(--icon-stroke, inherit)"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+        stroke="var(--icon-stroke, inherit)"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+    </svg>
+  ),
+);
+
+ArrangeCircle2Icon.displayName = "ArrangeCircle2Icon";
+
+export default ArrangeCircle2Icon;
