9819d6bc5ca5a7c4ff386d254b11adc26397d77c
Blair McKee <23467862+eblairmckee@users.noreply.github.com>
2025-02-24 10:20:40 -0800

    Update arrow-right-2 icon

---
+++ src/icons/ArrowRight2Icon.tsx
@@ -0,0 +1,41 @@
+import React, { forwardRef, type SVGProps } from "react";
+
+const ArrowRight2Icon = forwardRef<SVGSVGElement, SVGProps<SVGSVGElement>>(
+  (props, ref) => (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      ref={ref}
+      {...props}
+    >
+      <path
+        d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+        stroke="var(--icon-stroke, inherit)"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        d="M8.5 12H14.5"
+        stroke="var(--icon-stroke, inherit)"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        d="M12.5 15L15.5 12L12.5 9"
+        stroke="var(--icon-stroke, inherit)"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+    </svg>
+  ),
+);
+
+ArrowRight2Icon.displayName = "ArrowRight2Icon";
+
+export default ArrowRight2Icon;
